8c5d4b9dc5d636bd5901145f90bc11538b3a261c
Ivan Ajenjo <ivan.ajenjo.vicente@gmail.com>
2020-08-02 20:15:32 +0200

    Cambios del repo

--- dist/controllers/api.controllers.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.home = void 0;
-exports.home = (req, res) => {
-    return res.send('Home');
-};
--- dist/routes/api.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = express_1.Router();
-const api_controller_1 = require("../controllers/api.controller");
-router.get('/', api_controller_1.home);
-exports.default = router;
