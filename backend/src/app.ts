d5d3668d10d5f41d002ffd3beac2239110460216
Ivan <ivan.ajenjo.vicente@gmail.com>
2020-10-27 11:37:27 +0100

    Cambios de capturas de pantalla

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,35 +1,41 @@
 import express from "express";
 import morgan from "morgan";
-import path from 'path';
+import path from "path";
 import { getCapturas } from "./libs/scraper";
-import cron, {  } from "node-cron";
+import cron from "node-cron";
 
-import apiroutes from './routes/api.routes';
-import entradasroutes from './routes/entradas.routes';
+import apiroutes from "./routes/api.routes";
+import entradasroutes from "./routes/entradas.routes";
 
 //inicializaciones
 const app = express();
 
 //configuraciones
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
 //middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 //Tareas Programadas Por horas
-cron.schedule("* 9,15,22 * * *", async function(){
-    console.log("Realizando capturas");
-    getCapturas();
+cron.schedule("* 9,15,22 * * *", async function () {
+  console.log("Realizando capturas");
+  getCapturas();
 });
 
 //rutas
-app.get('/', (req, res) => {
-    res.send('Hola Mundo');
+app.get("/", (req, res) => {
+  res.send("Hola Mundo");
 });
 
-app.use('/capturas', express.static(path.resolve('capturas')));
-app.use('/home', apiroutes);
-app.use('/entradas', entradasroutes);
+app.get("/realizarCapturas", (req, res) => {
+    res.send("Realizando Capturas");
+    getCapturas();
+    console.log("Capturas Realizadas");
+});
 
-export default app
+app.use("/capturas", express.static(path.resolve("capturas")));
+app.use("/home", apiroutes);
+app.use("/entradas", entradasroutes);
+
+export default app;
--- backend/src/libs/scraper.ts
+++ backend/src/libs/scraper.ts
@@ -1,26 +1,27 @@
 import puppeteer, { Browser, Page } from "puppeteer";
 
-export function getCapturas(){
-    captElPais();
-    captElMundo();
+export function getCapturas() {
+  captElPais();
+  captElMundo();
 }
 
-async function captElPais(){
-    const browser: Browser = await puppeteer.launch();
-    const page: Page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 800 })
-    await page.goto('https://www.elpais.com/');	
-    //Aceptar Cookies
-    //await page.click('#didomi-notice-agree-button');
-    await page.screenshot({ path: 'temp/elpais.png', fullPage: true })
-    await browser.close();
+async function captElPais() {
+  const browser: Browser = await puppeteer.launch();
+  const page: Page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.goto("https://www.elpais.com/");
+  //Aceptar Cookies
+  await page.screenshot({ path: "temp/elpais.png", fullPage: true });
+  await browser.close();
 }
 
-async function captElMundo(){
-    const browser: Browser = await puppeteer.launch();
-    const page: Page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 800 })
-    await page.goto('https://www.elmundo.es/');	
-    await page.screenshot({ path: 'temp/elmundo.png', fullPage: true })
-    await browser.close();    
+async function captElMundo() {
+  const browser: Browser = await puppeteer.launch();
+  const page: Page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.goto("https://www.elmundo.es/");
+  //Aceptar cookies
+  await page.click("#didomi-notice-agree-button");
+  await page.screenshot({ path: "temp/elmundo.png", fullPage: true });
+  await browser.close();
 }
